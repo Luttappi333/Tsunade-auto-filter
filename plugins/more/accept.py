3e6111cd0f2cd110a2a9996bc4262a6d67023843
Muhammedshifin <muhammedshifink4@gmail.com>
2023-01-28 17:31:22 +0530

    Create accept.py

---
+++ plugins/more/accept.py
@@ -0,0 +1,34 @@
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+from pyrogram import Client, filters, errors, enums
+from asyncio import sleep
+from pyrogram.errors import UserNotParticipant
+from pyrogram.errors.exceptions.flood_420 import FloodWait
+import random, asyncio
+from pyrogram.types import Message, User, ChatJoinRequest
+
+
+gif = [
+    'https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4',
+    'https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4',
+    'https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4',
+    'https://telegra.ph/file/293cc10710e57530404f8.mp4',
+    'https://telegra.ph/file/506898de518534ff68ba0.mp4',
+    'https://telegra.ph/file/dae0156e5f48573f016da.mp4',
+    'https://telegra.ph/file/3e2871e714f435d173b9e.mp4',
+    'https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4',
+    'https://telegra.ph/file/876edfcec678b64eac480.mp4',
+    'https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4',
+    'https://telegra.ph/file/b4834b434888de522fa49.mp4'
+]
+
+
+@Client.on_chat_join_request(filters.group | filters.channel & ~filters.private)
+async def approve(client: Client, message: Message):
+    chat=message.chat # Chat
+    user=message.from_user # User
+    print(f"{user.first_name} 𝙹𝙾𝙸𝙽𝙴𝙳 ⚡") # Logs
+    await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
+    img = random.choice(gif)
+    #nothingenter
+    await client.send_video(user.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @KOMBOTZZ__ #Feutere By @Master_broi**".format(message.from_user.mention, message.chat.title))
+#by master
