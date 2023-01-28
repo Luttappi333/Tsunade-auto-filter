5fc0d4eb5dff37dbeeeb5e5e0c7c421821ea3bd3
Mayavi333 <105354161+Mayavi333@users.noreply.github.com>
2023-01-28 18:38:21 +0530

    Update accept.py

--- plugins/more/accept.py
+++ plugins/more/accept.py
@@ -8,17 +8,17 @@ from pyrogram.types import Message, User, ChatJoinRequest
 
 
 gif = [
-    'https://telegra.ph/file/a5a2bb456bf3eecdbbb99.mp4',
-    'https://telegra.ph/file/03c6e49bea9ce6c908b87.mp4',
-    'https://telegra.ph/file/9ebf412f09cd7d2ceaaef.mp4',
-    'https://telegra.ph/file/293cc10710e57530404f8.mp4',
-    'https://telegra.ph/file/506898de518534ff68ba0.mp4',
-    'https://telegra.ph/file/dae0156e5f48573f016da.mp4',
-    'https://telegra.ph/file/3e2871e714f435d173b9e.mp4',
-    'https://telegra.ph/file/714982b9fedfa3b4d8d2b.mp4',
-    'https://telegra.ph/file/876edfcec678b64eac480.mp4',
-    'https://telegra.ph/file/6b1ab5aec5fa81cf40005.mp4',
-    'https://telegra.ph/file/b4834b434888de522fa49.mp4'
+    'https://telegra.ph/file/7e38c0e9a6b6051199f92.mp4',
+    'https://telegra.ph/file/6d6ad78238403648013c4.mp4',
+    'https://telegra.ph/file/7e38c0e9a6b6051199f92.mp4',
+    'https://telegra.ph/file/6d6ad78238403648013c4.mp4',
+    'https://telegra.ph/file/7e38c0e9a6b6051199f92.mp4',
+    'https://telegra.ph/file/6d6ad78238403648013c4.mp4',
+    'https://telegra.ph/file/7e38c0e9a6b6051199f92.mp4',
+    'https://telegra.ph/file/6d6ad78238403648013c4.mp4',
+    'https://telegra.ph/file/7e38c0e9a6b6051199f92.mp4',
+    'https://telegra.ph/file/6d6ad78238403648013c4.mp4',
+    'https://telegra.ph/file/7e38c0e9a6b6051199f92.mp4'
 ]
 
 
@@ -30,5 +30,5 @@ async def approve(client: Client, message: Message):
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     img = random.choice(gif)
     #nothingenter
-    await client.send_video(user.id,img, "**Hello {}!\nWelcome To {}\n\n__Powerd By : @KOMBOTZZ__ \n\n#Feutere By @Master_broi**".format(message.from_user.mention, message.chat.title))
+    await client.send_video(user.id,img, "**Hello {}!\nYour Request To Join {} was approved👍\n\n__Powered By: @KOMBOTZZ __ \n\n#Feature By @Master_broi**".format(message.from_user.mention, message.chat.title))
 #by master
