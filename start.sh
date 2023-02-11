c1e9c60e09093fca47d9f237fd87d54885666b70
Mayavi333 <105354161+Mayavi333@users.noreply.github.com>
2023-02-11 13:05:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Mayavi333/Tsunade-auto-filter.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-The-File-Donor
 fi
 cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting ᴛꜱᴜɴᴀᴅᴇ⚡️...."
 python3 bot.py
