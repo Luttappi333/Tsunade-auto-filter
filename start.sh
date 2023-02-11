bb1261b20ef13a0c1552d04894005027d7a69db2
Mayavi333 <105354161+Mayavi333@users.noreply.github.com>
2023-02-11 18:11:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mayavi333/Tsunade-auto-filter.git /Tsunade-auto-filter
+  git clone https://github.com/Mayavi333/Tsunade-auto-filter.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Tsunade-auto-filter
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor
 fi
-cd /Tsunade-auto-filter
+cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
 echo "Starting ᴛꜱᴜɴᴀᴅᴇ⚡️...."
 python3 bot.py
