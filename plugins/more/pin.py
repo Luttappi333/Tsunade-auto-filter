0913b43dbcd242941fc840e3941e9b1f742f9401
Mayavi333 <105354161+Mayavi333@users.noreply.github.com>
2023-02-11 23:35:09 +0530

    Update pin.py

--- plugins/more/pin.py
+++ plugins/more/pin.py
@@ -1,15 +1,15 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from plugins.helper_functions.admin_check import admin_filter                         
+from plugins.helper_functions.admin_check import admin_fliter                         
 
-@Client.on_message(filters.command("pin") & admin_filter)
+@Client.on_message(filters.command("pin") & admin_fliter)
 async def pin(_, message: Message):
     if not message.reply_to_message:
         return
     await message.reply_to_message.pin()
 
 
-@Client.on_message(filters.command("unpin") & admin_filter)             
+@Client.on_message(filters.command("unpin") & admin_fliter)             
 async def unpin(_, message: Message):
     if not message.reply_to_message:
         return
