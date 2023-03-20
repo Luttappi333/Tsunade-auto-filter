4a5bfea6c3ef9199388d47acb70d48af4d822e14
Muhammedshifin <muhammedshifink4@gmail.com>
2023-03-20 09:00:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -613,6 +613,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ]
             ]
         )
+    )                                   
     elif query.data == "pages":
         await query.answer()
 
